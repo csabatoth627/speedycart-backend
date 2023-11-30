2b6442c3617c7b64142e192def5a5dcbf66baa39
Csaba <csabi627@gmail.com>
2023-11-30 10:40:39 +0100

    feat:create routes and protec these routes

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const port = process.env.PORT;
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
 
 app.get("/", (req, res) => {
