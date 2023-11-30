f7b031f68df5b3411fb2e78d91223627d8be8f98
Csaba <csabi627@gmail.com>
2023-11-30 11:36:35 +0100

    feat:complite addOrderItem endpoint

--- controller/orderController.js
+++ controller/orderController.js
@@ -2,8 +2,41 @@ import asyncHandler from "../middleware/asyncHandler.js";
 import Order from "../models/orderModel.js";
 
 const addOrderItems = asyncHandler(async (req, res) => {
-    res.send("Add order items");
+    const { orderItems,
+        shippingAddress,
+        paymentMethod,
+        itemsPrice,
+        taxPrice,
+        shippingPrice,
+        totalPrice,
+    } = req.body
+
+    if(orderItems && orderItems.length === 0){
+        res.status(400);
+        throw new Error("No order item")
+    } else {
+        const order = new Order({
+            orderItems: orderItems.map((item) => ({
+                ...item,
+                product: item._id,
+                _id:undefined
+            })),
+            user: req.user._id,
+            shippingAddress,
+            paymentMethod,
+            itemsPrice,
+            taxPrice,
+            shippingPrice,
+            totalPrice,
+        });
+
+        const createOrder = await order.save();
+
+        res.status(201).json(createOrder)
+    }
 });
+
+
 
 const getMyOrders = asyncHandler(async (req, res) => {
     res.send("Get my orders");
@@ -25,7 +58,7 @@ const getOrders = asyncHandler(async (req, res) => {
     res.send("Get orders");
 });
 
-export{
+export {
     addOrderItems,
     getMyOrders,
     getOrderById,
