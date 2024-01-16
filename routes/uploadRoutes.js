629d86e086592d55d6c0518c299201b425a72337
Csaba <csabi627@gmail.com>
2024-01-16 13:45:42 +0100

    feat: create storage (uploadRoutes.js)

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -4,4 +4,13 @@ import multer from 'multer'
 
 const router = express.Router();
 
+const storage = multer.diskStorage({
+    destination(req, file, cb) {
+        cb(null, '/uploads')
+    },
+    filename(req, res, cb) {
+        cb(null, `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`)
+    }
+})
+
 export default router;
