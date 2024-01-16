1cb2215dc945752af42c636eb924754519c66a9b
Csaba <csabi627@gmail.com>
2024-01-16 10:56:33 +0100

    feat:create updateProduct function and exported it (productConroller.js)

--- controller/productController.js
+++ controller/productController.js
@@ -16,12 +16,12 @@ const getProductById = asyncHandler(async (req, res) => {
     }
 })
 
-const createProduct = asyncHandler(async(req,res) => {
+const createProduct = asyncHandler(async (req, res) => {
     const product = new Product({
         name: 'Sample name',
         price: 0,
         user: req.user._id,
-        image: '/images/sample/.jpg',
+        image: '/images/sample.jpg',
         brand: 'Sample brand',
         category: 'Sample category',
         countInStock: 0,
@@ -33,4 +33,24 @@ const createProduct = asyncHandler(async(req,res) => {
     res.status(201).json(createProduct)
 })
 
-export {getProducts, getProductById,createProduct}
+const updateProduct = asyncHandler(async (req, res) => {
+    const { name, price, description, image, brand, category, countInStock } = req.body
+    const product = await Product.findById(req.params.id)
+    if (product){
+        product.name = name;
+        product.price = price;
+        product.description = description;
+        product.image = image;
+        product.brand = brand;
+        product.category = category;
+        product.countInStock = countInStock;
+
+        const updatedProduct = await product.save();
+        res.json(updateProduct)
+    }else {
+        res.status(404);
+        throw new Error('Resource not found');
+    }
+})
+
+export { getProducts, getProductById, createProduct, updateProduct }
