2669f5aabcb08524ac4f4e0548b344267321e6aa
Csaba <csabi627@gmail.com>
2024-02-02 13:04:46 +0100

    feat: create createProductReview (productController.js)

--- controller/productController.js
+++ controller/productController.js
@@ -36,7 +36,7 @@ const createProduct = asyncHandler(async (req, res) => {
 const updateProduct = asyncHandler(async (req, res) => {
     const { name, price, description, image, brand, category, countInStock } = req.body
     const product = await Product.findById(req.params.id)
-    if (product){
+    if (product) {
         product.name = name;
         product.price = price;
         product.description = description;
@@ -47,7 +47,7 @@ const updateProduct = asyncHandler(async (req, res) => {
 
         const updatedProduct = await product.save();
         res.json(updateProduct)
-    }else {
+    } else {
         res.status(404);
         throw new Error('Resource not found');
     }
@@ -56,13 +56,48 @@ const updateProduct = asyncHandler(async (req, res) => {
 const deleteProduct = asyncHandler(async (req, res) => {
     const product = await Product.findById(req.params.id)
 
-    if(product){
-        await Product.deleteOne({_id: product._id});
-        res.status(200).json({message: 'Product deleted'})
+    if (product) {
+        await Product.deleteOne({ _id: product._id });
+        res.status(200).json({ message: 'Product deleted' })
     } else {
         res.status(404);
         throw new Error('Resource not found')
     }
 })
 
-export { getProducts, getProductById, createProduct, updateProduct, deleteProduct }
+const createProductReview = asyncHandler(async (req, res) => {
+    const { rating, comment } = req.body
+
+    const product = await Product.findById(req.params.id)
+
+    if (product) {
+        const alreadyReviewed = product.reviews.find((review) => {
+            review.user.toString() === req.user._id.toString()
+        });
+
+        if (alreadyReviewed) {
+            res.status(400);
+            throw new Error('Product already reviewed')
+        }
+
+        const review = {
+            name: req.user.name,
+            rating: Number(rating),
+            comment,
+            user: req.user._id,
+        };
+
+        product.reviews.push(review);
+
+        product.numReviews = product.reviews.length;
+
+        await product.save();
+        res.status(201).json({ message: 'Review added' })
+
+    } else {
+        res.status(404);
+        throw new Error('Product not found')
+    }
+})
+
+export { getProducts, getProductById, createProduct, updateProduct, deleteProduct, createProductReview }
